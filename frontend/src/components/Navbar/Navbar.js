d61fc0979137872bd4b30a3270d407a3d39f1954
Steve Kimoi <moenisteve@gmail.com>
2023-05-19 09:17:20 +0300

    creating components and initializing navbar component

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,66 @@
+import './Navbar.css'
+import { useEffect } from 'react';
+import { web3auth } from '../../ConnectWallet/ConnectWallet';
+
+function Navbar({ openModal, account, walletConnected, setWalletConnected, setAccount  }) {
+
+  // Connecting wallet 
+  const handleConnect = async () => {
+    try {
+       await web3auth.initModal(); 
+
+       await web3auth.connect(); 
+    } catch (error){
+        console.error(error)
+    }
+  }   
+
+  // Disconnect connected wallet 
+  const handleDisconnect = () => {
+    try {
+      setWalletConnected(false); 
+      setAccount(""); 
+      console.log("Account disconnected!"); 
+    } catch (error){
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    // Console log the account connected
+    console.log("Account is: ", account); 
+  })
+
+
+  return (
+      <div className="navbar">
+        <div className="logo">
+          <p>DappStarterKit</p>
+        </div>
+
+        {
+          !walletConnected && (
+            <button className="connect-button" onClick={handleConnect} >Connect Wallet</button>
+          )
+        }
+
+        {
+          walletConnected && (
+            <div>
+              {
+                account && (
+                  <button className='connect-button'>
+                    { account.slice(0,6) + "..." + account.slice(38,42) }
+                  </button>
+                )
+              }
+              <button className='connect-button' onClick={handleDisconnect}>Disconnect Wallet</button>
+            </div>
+          )
+        }
+        
+      </div>
+  );
+}
+
+export default Navbar;
